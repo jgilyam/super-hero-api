91d2370c9d0792f96068e8cea877dc056aff8fe7
jgilyam <jgilyam@folcode.com>
2023-03-13 14:27:26 -0300

    add function

--- src/main/java/com/folcode/super_hero_api/services/SuperHeroService.java
+++ src/main/java/com/folcode/super_hero_api/services/SuperHeroService.java
@@ -1,2 +1,27 @@
-package com.folcode.super_hero_api.services.validators;public class SuperHeroService {
+package com.folcode.super_hero_api.services;
+
+import com.folcode.super_hero_api.domain.dtos.SuperHeroDTO;
+import com.folcode.super_hero_api.domain.mappers.ISuperHeroMapper;
+import com.folcode.super_hero_api.domain.persistence.entities.SuperHeroEntity;
+import com.folcode.super_hero_api.domain.persistence.repositories.SuperHeroRepository;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SuperHeroService {
+    private final SuperHeroRepository superHeroRepository;
+    private final ISuperHeroMapper superHeroMapper;
+
+    public SuperHeroService(SuperHeroRepository superHeroRepository, ISuperHeroMapper superHeroMapper) {
+        this.superHeroRepository = superHeroRepository;
+        this.superHeroMapper = superHeroMapper;
+    }
+
+    public List<SuperHeroDTO> findAll() {
+        return null;
+    }
 }
