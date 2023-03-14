7fd1ca9a9c2ea2a8f75e4da8fef2eaad63354d02
jgilyam <jgilyam@folcode.com>
2023-03-14 09:50:44 -0300

    using specification for build a "where"

--- src/main/java/com/folcode/super_hero_api/services/SuperHeroService.java
+++ src/main/java/com/folcode/super_hero_api/services/SuperHeroService.java
@@ -4,12 +4,12 @@ import com.folcode.super_hero_api.domain.dtos.SuperHeroDTO;
 import com.folcode.super_hero_api.domain.mappers.ISuperHeroMapper;
 import com.folcode.super_hero_api.domain.persistence.entities.SuperHeroEntity;
 import com.folcode.super_hero_api.domain.persistence.repositories.SuperHeroRepository;
+import com.folcode.super_hero_api.domain.persistence.specifications.SuperHeroSpecification;
 import com.folcode.super_hero_api.exceptions.SuperHeroNotFoundExceptions;
 import lombok.SneakyThrows;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.parser.Entity;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,14 +17,17 @@ import java.util.stream.Collectors;
 public class SuperHeroService {
     private final SuperHeroRepository superHeroRepository;
     private final ISuperHeroMapper superHeroMapper;
+    private final SuperHeroSpecification superHeroSpecifications;
 
-    public SuperHeroService(SuperHeroRepository superHeroRepository, ISuperHeroMapper superHeroMapper) {
+    public SuperHeroService(SuperHeroRepository superHeroRepository, ISuperHeroMapper superHeroMapper, SuperHeroSpecification superHeroSpecifications) {
         this.superHeroRepository = superHeroRepository;
         this.superHeroMapper = superHeroMapper;
+        this.superHeroSpecifications = superHeroSpecifications;
     }
 
-    public List<SuperHeroDTO> findAll() {
-        return superHeroRepository.findAll()
+    public List<SuperHeroDTO> findAll(String name) {
+        Specification<SuperHeroEntity> where = superHeroSpecifications.findAll(name);
+        return superHeroRepository.findAll(where)
                 .stream()
                 .map(superHeroMapper::superHeroEntityToSuperHeroDTO)
                 .collect(Collectors.toList());
