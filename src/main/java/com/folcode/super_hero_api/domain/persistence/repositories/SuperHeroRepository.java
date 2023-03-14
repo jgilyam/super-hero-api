ef70e0b33c5bab58147c5a817d58e341a3f22c6a
jgilyam <jgilyam@folcode.com>
2023-03-14 09:50:03 -0300

    add specification

--- src/main/java/com/folcode/super_hero_api/domain/persistence/repositories/SuperHeroRepository.java
+++ src/main/java/com/folcode/super_hero_api/domain/persistence/repositories/SuperHeroRepository.java
@@ -2,8 +2,12 @@ package com.folcode.super_hero_api.domain.persistence.repositories;
 
 import com.folcode.super_hero_api.domain.persistence.entities.SuperHeroEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface SuperHeroRepository extends JpaRepository<SuperHeroEntity,Integer> {
+public interface SuperHeroRepository extends JpaRepository<SuperHeroEntity,Integer>, JpaSpecificationExecutor<SuperHeroEntity> {
+    List<SuperHeroEntity> findAllByName(String name);
 }
---
+++ src/main/java/com/folcode/super_hero_api/domain/persistence/specifications/SuperHeroSpecification.java
@@ -0,0 +1,42 @@
+package com.folcode.super_hero_api.domain.persistence.specifications;
+
+import com.folcode.super_hero_api.domain.persistence.entities.SuperHeroEntity;
+
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class SuperHeroSpecification {
+    public Specification<SuperHeroEntity> findAll(String name) {
+        return (root, query, criteriaBuilder) -> findByAllQueryBuilder(root, criteriaBuilder, name);
+    }
+    private Predicate findByAllQueryBuilder(Root<SuperHeroEntity> agencyMetadata, CriteriaBuilder criteriaBuilder, String name){
+
+        Predicate where = criteriaBuilder.conjunction();
+
+        List<Predicate> predicates = new ArrayList<>();
+
+        Optional
+                .ofNullable(name)
+                .ifPresent(value -> predicates.add(namePredicate(agencyMetadata, criteriaBuilder, value)));
+
+        for (Predicate predicate : predicates)
+            where = criteriaBuilder.and(where, predicate);
+
+        return where;
+    }
+
+    private Predicate namePredicate(Root<SuperHeroEntity> root, CriteriaBuilder criteriaBuilder, String name) {
+        return criteriaBuilder.like(criteriaBuilder.lower(root.get("")).as(String.class), "%"+name.toLowerCase()+"%");
+    }
+}
