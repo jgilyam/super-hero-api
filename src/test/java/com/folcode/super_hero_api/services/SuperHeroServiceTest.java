2e92896dac7aa114384a1981cc68a2350ba419b5
jgilyam <jgilyam@folcode.com>
2023-03-14 15:10:55 -0300

    ad test for finAll with filter

--- src/test/java/com/folcode/super_hero_api/services/SuperHeroServiceTest.java
+++ src/test/java/com/folcode/super_hero_api/services/SuperHeroServiceTest.java
@@ -4,7 +4,10 @@ import com.folcode.super_hero_api.domain.dtos.SuperHeroDTO;
 import com.folcode.super_hero_api.domain.mappers.ISuperHeroMapper;
 import com.folcode.super_hero_api.domain.persistence.entities.SuperHeroEntity;
 import com.folcode.super_hero_api.domain.persistence.repositories.SuperHeroRepository;
+import com.folcode.super_hero_api.domain.persistence.specifications.ISuperHeroSpecification;
+import com.folcode.super_hero_api.domain.persistence.specifications.SuperHeroSpecification;
 import com.folcode.super_hero_api.exceptions.SuperHeroNotFoundExceptions;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,10 +15,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -24,13 +30,20 @@ import static org.mockito.Mockito.mock;
 public class SuperHeroServiceTest {
     @Mock
     private SuperHeroRepository superHeroRepositoryMock;
+
     @Mock
-    private ISuperHeroMapper superHeroMapperMock;
+    private  ISuperHeroMapper superHeroMapperMock;
+
     private SuperHeroService superHeroService;
+
+    @Mock
+    private ISuperHeroSpecification superHeroSpecificationsMock;
+
+
 
     @BeforeEach
     public void setup() {
-        superHeroService = new SuperHeroService(superHeroRepositoryMock, superHeroMapperMock);
+        superHeroService = new SuperHeroService(superHeroRepositoryMock, superHeroMapperMock,superHeroSpecificationsMock);
     }
 
     @Test
@@ -39,15 +52,21 @@ public class SuperHeroServiceTest {
         superHeroesExpected.add(new SuperHeroEntity(1, "Spiderman", null, null, null, null));
         superHeroesExpected.add(new SuperHeroEntity(2, "Spiderman2", null, null, null, null));
 
-        Mockito
-                .when(superHeroRepositoryMock.findAll())
-                .thenReturn(superHeroesExpected);
+        Specification<SuperHeroEntity> whereMock= (root, query, criteriaBuilder) -> criteriaBuilder.conjunction();
 
+        Mockito
+                .when(superHeroSpecificationsMock.findAll(null))
+                .thenReturn(whereMock);
+
+        Mockito
+                .when(superHeroRepositoryMock.findAll(whereMock))
+                .thenReturn(superHeroesExpected);
         Mockito
                 .when(superHeroMapperMock.superHeroEntityToSuperHeroDTO(any(SuperHeroEntity.class)))
                 .thenReturn(new SuperHeroDTO());
 
-        List<SuperHeroDTO> superHeroesReturned = superHeroService.findAll();
+
+        List<SuperHeroDTO> superHeroesReturned = superHeroService.findAllSuperHeroes(null);
 
         Assertions.assertThat(superHeroesReturned.size()).isEqualTo(superHeroesExpected.size());
 
@@ -62,12 +81,14 @@ public class SuperHeroServiceTest {
         Mockito
                 .when(superHeroRepositoryMock.findById(42))
                 .thenReturn(Optional.ofNullable(mockEntity));
+
         Mockito
                 .when(superHeroMapperMock.superHeroEntityToSuperHeroDTO(mockEntity))
                 .thenReturn(mockDto);
 
 
-        SuperHeroDTO superHero = superHeroService.findById(42);
+
+        SuperHeroDTO superHero = superHeroService.findSuperHeroById(42);
 
         Assertions.assertThat(superHero)
                 .isSameAs(mockDto);
@@ -82,8 +103,51 @@ public class SuperHeroServiceTest {
                 .thenReturn(Optional.empty());
 
         Assertions
-                .assertThatThrownBy(()->superHeroService.findById(43))
+                .assertThatThrownBy(()->superHeroService.findSuperHeroById(43))
                 .isInstanceOf(SuperHeroNotFoundExceptions.class);
+
+    }
+    @Test
+    public void shouldReturnAllSuperHeroesByName(){
+        List<SuperHeroEntity> superHeroesExpected = new ArrayList<>();
+        superHeroesExpected.add(new SuperHeroEntity(1, "Spiderman", null, null, null, null));
+        superHeroesExpected.add(new SuperHeroEntity(2, "Superman", null, null, null, null));
+        superHeroesExpected.add(new SuperHeroEntity(3, "Manolito el fuerte", null, null, null, null));
+
+        List<SuperHeroDTO> superHeroesDtoExpected = new ArrayList<>();
+        superHeroesDtoExpected.add(new SuperHeroDTO(1, "Spiderman", null, null, null, null));
+        superHeroesDtoExpected.add(new SuperHeroDTO(2, "Superman", null, null, null, null));
+        superHeroesDtoExpected.add(new SuperHeroDTO(3, "Manolito el fuerte", null, null, null, null));//superHeroesDtoExpected.add(new SuperHeroDTO(4, "Dr. Strange", null, null, null, null));
+
+        String queryName = "man";
+
+        Specification<SuperHeroEntity> whereMock= (root, query, criteriaBuilder) -> criteriaBuilder.and(criteriaBuilder.conjunction(), criteriaBuilder.like(criteriaBuilder.lower(root.get("name")).as(String.class), "%"+queryName.toLowerCase()+"%"));
+
+        Mockito
+                .when(superHeroSpecificationsMock.findAll(queryName))
+                .thenReturn(whereMock);
+
+        Mockito
+                .when(superHeroRepositoryMock.findAll(whereMock))
+                .thenReturn(superHeroesExpected);
+
+        for (int i = 0; i < superHeroesExpected.size(); i++) {
+            Mockito
+                    .when(superHeroMapperMock.superHeroEntityToSuperHeroDTO(superHeroesExpected.get(i)))
+                    .thenReturn(superHeroesDtoExpected.get(i));
+
+        }
+
+
+
+        List<SuperHeroDTO> superHeroesReturned = superHeroService.findAllSuperHeroes(queryName);
+
+
+        Assertions
+                .assertThat(superHeroesReturned)
+                .extracting(SuperHeroDTO::getId)
+                .containsOnly(1,2,3);
+
 
     }
 }
